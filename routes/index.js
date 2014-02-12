085bd85820064624a910f7cada3cf6be21070191
Sam Stefan <sam@synthmedia.co.uk>
2014-02-12 13:18:09 +0000

    test

--- routes/index.js
+++ routes/index.js
@@ -13,10 +13,10 @@ module.exports.loadRoutes = function (serviceLocator, app) {
 
   // Post requests for commits..
   app.post('/get-commit', function(req, res){
-    var img = req.files.path
+    var img = req.files.file.path
 
     console.log('img:', img)
-    fs.readFile(req.files.path, function (err, data) {
+    fs.readFile(req.files.file.path, function (err, data) {
       var newPath = __dirname + '/uploads/uploadedFileName'
       fs.writeFile(newPath, data, function (err) {
         res.redirect('back')
