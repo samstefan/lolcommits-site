a6698fc7aaaa89b89be0c6a07b632b5e3e465a4e
Sam Stefan <sam@dpov.co.uk>
2014-05-18 01:42:47 +0100

    Added slack integration.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var path = require('path')
   , fs = require('fs')
   , uploadPath = path.normalize(__dirname + '/../public/uploads/')
+  , Slack = require('node-slack')
+  , slack = new Slack('','')
 
 module.exports.loadRoutes = function (serviceLocator, app) {
 
@@ -18,17 +20,29 @@ module.exports.loadRoutes = function (serviceLocator, app) {
     var dateTaken = new Date(req.files.file.lastModifiedDate)
 
     fs.readFile(req.files.file.path, function (err, data) {
-      if (err) throw err
-      var newPath = uploadPath + dateTaken.toJSON() + '-' +  req.files.file.name
-      fs.writeFile(newPath, data, function (err) {
-        if (err) throw err;
-        serviceLocator.logger.info('Saved image')
-        res.json(
-          { 'status': 'success'
-          , 'message': 'Image Saved!'
+      if (err) {
+        serviceLocator.logger.error(err)
+      } else {
+        var newPath = uploadPath + dateTaken.toJSON() + '-' +  req.files.file.name
+        fs.writeFile(newPath, data, function (err) {
+          if (err) {
+            serviceLocator.logger.error(err)
+          } else {
+            serviceLocator.logger.info('Saved image')
+            res.json(
+              { 'status': 'success'
+              , 'message': 'Image Saved!'
+              }
+            )
+            // Send file to slack
+            slack.send({
+              text: 'http://lol.samstefan.co.uk' + newPath,
+              channel: '#random',
+              username: 'LOL Commits'
+            })
           }
-        )
-      })
+        })
+      }
     })
   })
 
